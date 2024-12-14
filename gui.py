28c27d95ff487b7117414eb5c15bd2f998ccbdbb
Robert Haywood <107866346+Haywood3@users.noreply.github.com>
2024-12-13 20:31:38 -0500

    Update basic gui without interactions

--- gui.py
+++ gui.py
@@ -1,12 +1,10 @@
 from modules import functions
-import PySimpleGUI as sg
+import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a To-Do")
 input_box = sg.InputText(tooltip="Enter To-Do")
 add_button = sg.Button("Add")
 
-window = sg.Window('To-Do App', layout=[[label, input_box, add_button]])
+window = sg.Window('To-Do App', layout=[[label], [input_box, add_button]])
 window.read()
 window.close()
-
-
--- modules/functions.py
+++ modules/functions.py
@@ -1,4 +1,4 @@
-FILEPATH = "todos.txt"
+FILEPATH = "./files/todos.txt"
 
 """Read a text file and return a list of text items"""
 def get_todos(filepath=FILEPATH):
